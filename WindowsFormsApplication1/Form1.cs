6071d6a59942d7b5ccda39ac731e41e7208ec577
Manuel Villegas <humbapumbamv@gmail.com>
2017-05-23 04:07:14 -0600

    Agregada el FrontEnd de la aplicación.

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,40 @@ namespace WindowsFormsApplication1
           {
                InitializeComponent();
           }
+
+          private void Form1_Load(object sender, EventArgs e)
+          {
+
+          }
+
+          private void button1_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void button2_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void radioButton2_CheckedChanged(object sender, EventArgs e)
+          {
+
+          }
+
+          private void label1_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void label3_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+          {
+
+          }
      }
 }
